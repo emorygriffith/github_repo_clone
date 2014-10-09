acd85473c9f1e39c16b4553c0aaeb3e5c30bb3a0
Emory Griffith <emorygriffith@Emorys-MacBook-Air.local>
2014-10-09 15:08:58 -0400

    got top and side .getJSON's done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,23 @@
-console.log('The Iron Yard Rocks');
+
+var users = 'https://api.github.com/users/emorygriffith';
+var repo = 'https://api.github.com/users/emorygriffith/repos';
+var org = 'https://api.github.com/users/emorygriffith/orgs';
+
+
+
+
+
+  var template = $('#top_box').html();
+  var rendered = _.template(template);
+
+  $.getJSON(users).done( function(i){
+    $('.headright').prepend(rendered(i));
+  });
+
+
+  var template2 = $('#side_box').html();
+  var rendered2 = _.template(template2);
+
+  $.getJSON(users).done( function(i){
+    $('.boxleft').prepend(rendered2(i));
+  });
